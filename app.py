5db61c9682f41523a6f4cd4981fc0661c16982f3
MichalPuch <puchalski.michal.t@gmail.com>
2018-10-12 16:43:15 +0200

    adding radnom class

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Let's start the game"
+    random_element = random.randint(0,9)
+    radomised_class = "Gnagster" if random_element > 7  else "Policeman" if random_element > 6 else "Citizen"
+    user = {'class': radomised_class}
+    return '''
+<html>
+    <head>
+        <title>Mafia Game - Your class</title>
+    </head>
+    <body>
+        <h1>You are a ''' + user['class'] + '''!</h1>
+    </body>
+</html>'''
+
 
 #!flask/bin/python
 if __name__ == '__main__':
