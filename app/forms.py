396091f3214485f1e96dae83c2fbc819a4b0c17d
MichalPuch <puchalski.michal.t@gmail.com>
2018-10-14 12:26:59 +0200

    deleting login button

--- app/forms.py
+++ app/forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min = 1, max = 10)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=1, max=10)])
     submit = SubmitField('Sign In')
--- app/routes.py
+++ app/routes.py
@@ -7,17 +7,19 @@ from app.forms import LoginForm
 @app.route('/')
 def go_to_login():
     return redirect('/login')
+
 @app.route('/index')
 def index():
     random_element = random.randint(0,9)
     radomised_class = "Gangster" if random_element > 7  else "Policeman" if random_element > 6 else "Citizen"
     user = {'class': radomised_class}
     return render_template('class_preview.html', user=user)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
+        flash('{}'.format(
             form.username.data))
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
