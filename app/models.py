0a9035d416e5594f95df588ae6ee863e9d41a5ca
MichalPuch <puchalski.michal.t@gmail.com>
2018-10-16 22:49:32 +0200

    adding migrations

--- app/models.py
+++ app/models.py
@@ -3,16 +3,16 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=False)
-    room_in = db.Column(db.Integer, index=True)
-    is_host = db.Column(db.Boolean, index=True)
+    room_in = db.Column(db.Integer, index=True, unique=False)
+    is_host = db.Column(db.Boolean, index=True, unique=False)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-class Room(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(4), index=True, unique=True)
-    host = db.Column(db.String(10), index=True, unique=False)
-
-    def __repr__(self):
-        return '<Room with code{} has a host {}>'.format(self.code, self.host)
+# class Room(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     code = db.Column(db.String(4), index=True, unique=True)
+#     host = db.Column(db.String(10), index=True, unique=False)
+#
+#     def __repr__(self):
+#         return '<Room with code{} has a host {}>'.format(self.code, self.host)
