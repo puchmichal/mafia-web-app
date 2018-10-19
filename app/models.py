c1c4bca3ce4995e4ec9c7a29171ce780e6b4c9e9
MichalPuch <puchalski.michal.t@gmail.com>
2018-10-19 19:33:52 +0200

    addign second table

--- app/models.py
+++ app/models.py
@@ -1,18 +1,29 @@
 from app import db
+from datetime import datetime
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=False)
-    room_in = db.Column(db.Integer, index=True, unique=False)
-    is_host = db.Column(db.Boolean, index=True, unique=False)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'))
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-# class Room(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     code = db.Column(db.String(4), index=True, unique=True)
-#     host = db.Column(db.String(10), index=True, unique=False)
-#
-#     def __repr__(self):
-#         return '<Room with code{} has a host {}>'.format(self.code, self.host)
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    room_name = db.Column(db.String(4), index=True, unique=True)
+    host_id = db.Column(db.Intiger, index=True, unique=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    users = db.relationship('Username', backref='client', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Room {}>'.format(self.room_name)
+
+
+
+
+
+    def __repr__(self):
+        return '<Room with code{} has a host {}>'.format(self.code, self.host)
