97cc69ba730e1d72fba59a86e6275eb8399972da
MichalPuch <puchalski.michal.t@gmail.com>
2018-10-24 21:53:26 +0200

    deleting sqllite

--- app/config.py
+++ app/config.py
@@ -3,6 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'mafia_will_get_you'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_ONYX_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_ONYX_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
