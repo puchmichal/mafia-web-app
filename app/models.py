d8fd1a754a3f0b67ddc757cc3e7c8108dae8e950
MichalPuch <puchalski.michal.t@gmail.com>
2018-10-24 19:58:48 +0200

    correct database try

--- app/models.py
+++ app/models.py
@@ -4,8 +4,9 @@ from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=False)
+    username = db.Column(db.String(64), index=True, unique=True)
     room_id = db.Column(db.Integer, db.ForeignKey('room.id'))
+    is_host = db.Column(db.Boolean)
 
 
     def __repr__(self):
@@ -14,16 +15,10 @@ class User(db.Model):
 class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     room_name = db.Column(db.String(4), index=True, unique=True)
-    host_id = db.Column(db.Intiger, index=True, unique=False)
+    host_id = db.Column(db.Integer)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    users = db.relationship('Username', backref='client', lazy='dynamic')
+    users = db.relationship('User', backref='client', lazy='dynamic')
 
     def __repr__(self):
-        return '<Room {}>'.format(self.room_name)
+        return '<Room with name {} has user {} as a host>'.format(self.room_name, self.host_id)
 
-
-
-
-
-    def __repr__(self):
-        return '<Room with code{} has a host {}>'.format(self.code, self.host)
